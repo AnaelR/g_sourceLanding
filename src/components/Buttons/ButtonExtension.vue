9070c92c0aa07a60d6298d9310137e8465f70216
AnaëlR <hey@anael-rodet.fr>
2022-04-25 15:24:18 +0200

    End header

--- src/components/Buttons/ButtonExtension.vue
+++ src/components/Buttons/ButtonExtension.vue
@@ -8,15 +8,17 @@ export default {};
 
 <style lang="scss" scoped>
 .cta-extension {
-  padding: 15px 30px;
+  padding: 10px 25px;
   background-color: $accent-color;
   color: $secondary-color;
   text-decoration: none;
   border-radius: 15px;
+  width: fit-content;
+  height: 31px;
   &::before {
     content: url("@/assets/icons/chrome.svg");
     display: inline-flex;
-    vertical-align: top;
+    vertical-align: middle;
     margin-right: 20px;
     scale: 0.8;
     margin-top: -2px;
